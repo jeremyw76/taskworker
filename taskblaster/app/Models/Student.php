46e5a11d64bd99f60b7933bfe84bc850b681f60e
jeremyw76 <jeremyw76@live.com>
2019-08-27 17:20:14 -0500

    Repos and Models
    - Added models and repos for the student, course and task tables.

---
+++ taskblaster/app/Models/Student.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $table = 'student';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
